e6d57251341c2d4e4cb4fee9375a0d75b92ab1a3
Rehan Qasim <qkrehan@gmail.com>
2019-06-25 22:20:03 +0500

    pop

---
+++ my_first_module.py
@@ -0,0 +1,12 @@
+def factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return num*factorial(num-1)
+    
+	
+	
+def combination(n,r):
+    return factorial(n)/(factorial(r)*factorial(n-r))
+		
+	
---
+++ paki.py
@@ -0,0 +1,2 @@
+def dob(a):
+    return bin(a)
